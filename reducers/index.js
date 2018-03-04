30f5f0cd82c5ef702f6fa0dca43f1a39250a8304
AndrewWellock <36522613+AndrewWellock@users.noreply.github.com>
2018-03-04 12:30:32 -0500

    Add files via upload

--- reducers/index.js
+++ reducers/index.js
@@ -2,7 +2,11 @@ import { combineReducers } from 'redux';
 import login from './loginReducer';
 import menu from './menuReducer';
 import profile from './profileReducer';
+import openChannel from './openChannelReducer';
 
 export default combineReducers({
-    login
+    login,
+    menu,
+    profile,
+    openChannel
 });
